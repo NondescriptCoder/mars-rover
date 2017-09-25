94651fb283f9011b6638fad49bd999baefd50496
Chris C <ccamer3940@greenvilleschools.us>
2017-09-25 10:31:10 -0400

    Rovers capable of  omnidirectional movement, as well as taking pictures  and destroying other rovers.

--- Rover.java
+++ Rover.java
@@ -8,71 +8,174 @@
 public class Rover
 {
     // fields
-    String name;
-    int x;
-    int y;
-    int dir; // 0=North, 1=East, 2=South, 3=West
-    
+    private String name;
+    private int x;
+    private int y;
+    private int dir; // 0=North, 1=East, 2=South, 3=West
+    private int numPics;
+    private boolean isFunctional;
     
     // constructor(s)
-    public Rover(String name)
+    // THIS MEANS THAT IT ACCESSES UNIVERSAL RATHER THAN LOCAL
+      public Rover()
     {
         this.name = name;
         this.x = 0;
         this.y = 0;
         this.dir = 0;
+        this.numPics = 0;
+        isFunctional = true;
+    }
+        public Rover(String name)
+    {
+        this.name = name;
+        this.x = 0;
+        this.y = 0;
+        this.dir = 0;
+        this.numPics = 0;
+        isFunctional = true;
+    }
+    public Rover(String name, int x, int y, int dir)
+    {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        isFunctional = true;
     }
     
+    public void  setName(String name)
+    {
+        this.name = name;
+    }
     
     // methods - stuff the Rover can do
     public void move()
     {
-        if (dir == 0)
+        if (isFunctional)
         {
-            y += 1;
-        }
-        else if (dir == 1)
-        {
-            x += 1;
-        }
-        else if (dir == 2)
-        {
-            y -= 1;
-        }
-        else 
-        {
-            x -= 1;
-        }
-        
+             if (dir == 0)
+                {
+                  y++;
+                }
+             if (dir == 1)
+                {
+                    y++;
+                    x++;
+                }
+             if (dir == 2)
+                {
+                    x++;
+                }
+             if (dir == 3)
+                {
+                    y--;
+                    x++;
+                }
+             if (dir == 4)
+                {
+                    y--;
+                }
+             if (dir == 5)
+                {
+                    y--;
+                    x--;
+                }
+             if (dir == 6)
+                {
+                    x--;
+                }
+             if (dir == 7)
+                {
+                    y++;
+                    x--;
+                }
         System.out.println(name + " moved in direction " + dir);
+    }
+        if (!isFunctional)
+        {
+         System.out.println("Do you seriously think a broken rover can still do things?!");
+        }
     }
     
     public void rotateLeft() 
     {
-        dir -= 1;
-        
-        if (dir < 0)
+        if (isFunctional)
         {
-            dir = 3;
-        }
-        
-        System.out.println(name + " turned to the left");        
+            dir -= 1;
+            
+            if (dir < 0)
+            {
+                dir = 7;
+            }
+            
+            System.out.println(name + " turned to the left");   
+    }
+        if (!isFunctional)
+        {
+         System.out.println("Do you seriously think a broken rover can still do things?!");
+        }    
     }
     
     public void rotateRight()
     {
-        dir += 1;
-        
-        if (dir == 4)
+        if (isFunctional)
         {
-            dir = 0;
-        }
-        
-        System.out.println(name + " turned to the right");        
+            dir += 1;
+            
+            if (dir == 8)
+            {
+                dir = 0;
+            }
+            
+            System.out.println(name + " turned to the right");    
+    }
+        if (!isFunctional)
+        {
+         System.out.println("Do you seriously think a broken rover can still do things?!");
+        }    
+    }
+    
+    public void takePic()
+    {
+        if (isFunctional){
+            String direction;
+            numPics++;
+            switch(dir){
+                case 0: direction = "North";
+                break;
+                case 1: direction = "Northeast";
+                break;
+                case 2: direction = "East";
+                break;
+                case 3: direction = "Southeast";
+                break; 
+                case 4: direction = "South";
+                break;
+                case 5: direction = "Southwest";
+                break;
+                case 6: direction = "West";
+                break;
+                case 7: direction = "Northwest";
+                break;             
+                default: direction = "Up";
+                break;
+            }     
+            System.out.println("Picture taken at [" + x + ", " + y + "] to the " + direction + " by " + name);
+    }
+        if (!isFunctional)
+        {
+         System.out.println("Do you seriously think a broken rover can still do things?!");
+        }    
     }
 
+    public void destroy(Rover other)
+    {
+        System.out.println(this.name + " obliterated " + other.name);
+        other.isFunctional = false;
+    }
     public String toString() 
     {
-        return "Rover[name=" + name + ", x=" + x + ", y=" + y + ", dir=" + dir + "]";
+        return "Rover[name=" + name + ", x=" + x + ", y=" + y + ", dir=" + dir + " picturesTaken=" +numPics+ " functional = " +isFunctional+ " ]\n";
     }
 }
--- RoverRunner.java
+++ RoverRunner.java
@@ -10,23 +10,43 @@ public class RoverRunner
     public static void main(String[] arrrggggggs)
     {
         Rover r1 = new Rover("Curiosity");
-        Rover r2 = new Rover("Spirit");
+        Rover r2 = new Rover("Spirit", 0, 0, 1);
+        Rover r3 = new Rover();
         
-        System.out.println(r1);
+        System.out.println(r3);
+        r3.setName("name");
+        System.out.println(r3);
         
-        r1.move();
-        System.out.println(r1);
-
-        r1.rotateRight();
-        System.out.println(r1);
-        
-        r2.rotateLeft();
-        System.out.println(r2);
-        
+        int count = 0;
+        while (count<5)
+        {
         r2.move();
         System.out.println(r2);
+        count++;
+        }
+        r2.rotateRight();
+        count = 0;
+        while (count<2)
+        {
+        r2.move();
+        System.out.println(r2);
+        count++;
+        }
         
+        r1.setName("Sojourner");
+         System.out.println(r1);
+        r1.move();
+         System.out.println(r1);
+        r1.rotateLeft();
+        System.out.println(r1);
         r1.move();
         System.out.println(r1);
-    }
+        r1.takePic();
+        System.out.println(r1);
+        
+        r1.destroy(r2);
+        r2.move();
+        System.out.println(r2);
+       
+}
 }
