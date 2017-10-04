19330f0d7ad4e23c8f538e45fa3c2f9cfdba154c
Chris C <ccamer3940@greenvilleschools.us>
2017-10-04 10:31:17 -0400

    Began scanner

--- RoverRunner.java
+++ RoverRunner.java
@@ -7,39 +7,18 @@
  */
 public class RoverRunner
 {
-    static int x;
+    
+    
     public static void main(String[] arrrggggggs)
     {
-        Rover r1 = new Rover("Curiosity");
-        Rover r2 = new Rover("Spirit", 0, 0, 2);
-        Rover r3 = new Rover();
-        
-        System.out.println(r3);
-        r3.setName("Sojourner");
-        System.out.println(r3);
-        
-        r2.takePic();
-        System.out.println(r2);
-        
-        r1.move();
-        System.out.println(r1);
-        r1.damage(r3);
-        System.out.println(r3);
-        System.out.println(r1);
-        r1.damage(r3);
-        System.out.println(r3);
-        System.out.println(r1);
-        r1.damage(r3);
-        System.out.println(r3);
-        
-        System.out.println(r1);
-        
-        r3.move();
-        System.out.println(r3);
-        
-        r1.moveTo(20, -17);
-        System.out.println(r1);
+        SimpleScanner input = new SimpleScanner();
+        Rover r1 = new Rover("Bob",0,0,0);
+      if (input.readString().equals("w"))
+      {
+          r1.move();
+      }
+      System.out.println(r1);
 
 
-}
+    }
 }
