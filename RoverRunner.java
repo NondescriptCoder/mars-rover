1df9edf32e5bd8328cfe77dca0b86a3eb00c11e4
Chris C <ccamer3940@greenvilleschools.us>
2017-10-06 10:31:04 -0400

    Improved interfaces, added abilities

--- RoverRunner.java
+++ RoverRunner.java
@@ -24,38 +24,70 @@ public class RoverRunner
 
         while (on)
         {
+            System.out.print("Input Rover Name Or Non-Rover Command: ");
             String name = input.readString();
             Rover r = group.find(name);
             if (r != null)
             {
-                if (input.readString().equals("exit"))
+                System.out.print("What do you want the rover to do?: ");
+                String  action = input.readString();
+
+                if (action.equals("w"))
+                {
+                    System.out.print ("How far?");
+                    int distance = input.readInt();
+                    r.move(distance);
+                }
+                else if (action.equals("d"))
+                {
+                    System.out.print ("How far?");
+                    int distance = input.readInt();
+                    r.rotate(distance);
+                }
+                else if(action.equals("a"))
+                {
+                    System.out.print ("How far?");
+                    int distance = input.readInt();
+                    r.rotate(distance*-1);
+                }
+                else if(action.equals("t"))
+                {
+                    System.out.print("What x-coordinate?");
+                    int x = input.readInt();
+                    System.out.print("y?");
+                    int y = input.readInt();
+                    r.teleport(x, y);
+                }
+                else if(action.equals("m"))
+                {
+                    System.out.print("What x-coordinate?");
+                    int x = input.readInt();
+                    System.out.print("y?");
+                    int y = input.readInt();
+                    r.moveTo(x, y);
+                }
+                else if(action.equals("p"))
+                {
+                    r.takePic();
+                }
+                    
+                System.out.println(r);
+            }
+            else
+            {
+                if (name.equals("exit"))
                 {
                     on = false;
                     System.out.println("Code Terminated");
                 }
-                if(input.readString().equals("help"))
+                else if(name.equals("help"))
                 {
-                    System.out.println("w = move forward\na = turn left\nd = turn right");
-                }
-                else if (input.readString().equals("w"))
+                    System.out.println("Rover Commands:\nw = move forward\na = turn left\nd = turn right\nt = teleport\nm = move to\np = take picture\nNon-Rover Commands:\nexit = end the program\n");
+                }                
+                else
                 {
-                    r.move();
+                   System.out.println("Rover name invalid");
                 }
-                else if (input.readString().equals("d"))
-                {
-                    int distance = input.readInt();
-                    r.rotateRight(distance);
-                }
-                else if(input.readString().equals("a"))
-                {
-                    int distance = input.readInt();
-                    r.rotateLeft(distance);
-                }
-                System.out.println(r1);
-            }
-            else
-            {
-              System.out.println("Rover name invalid");
             }
             }
         }
